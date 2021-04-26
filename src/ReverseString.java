371ec781e93e1bf16a0df1fb11e4ea2066c357ad
sayaliahirrao3@gmail.com <sayaliahirrao3@gmail.com>
2021-04-26 17:55:34 -0500

    first commit

---
+++ src/ReverseString.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+
+public class ReverseString {
+	public static void main(String[] args) {
+	
+	 String str = "Welcome to the ISU";
+	 
+	 String revString = "";
+	 {
+	 for (int i = str.length() - 1; i >= 0; --i)
+	 	{
+		   revString += str.charAt(i);
+	 	}
+	 System.out.println(revString);
+	}
+	
+	}
+}
+	
