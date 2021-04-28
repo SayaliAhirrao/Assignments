1b284dd89463924a2a3d767c8582ed137ee38ab4
sayaliahirrao3@gmail.com <sayaliahirrao3@gmail.com>
2021-04-28 17:35:53 -0500

    first commit

---
+++ src/StrToIntConersion.java
@@ -0,0 +1,14 @@
+
+public class StrToIntConersion {
+
+	public static void main(String args[]){
+		String str="35";
+		//nversion
+		 
+		int inum2 = Integer.parseInt(str);
+			
+		//int sum = inum+inum2;
+		System.out.println("Result is: " + inum2);
+	   }
+
+}
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,9 @@
 
 public class Student {
 	String name;
+	public Student(String string, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+	}
 	String getName(){
 		
 		return name;
